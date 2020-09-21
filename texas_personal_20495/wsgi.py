2f1a0dcbf905cf97b54452fb73b0269b0b3d1fe7
Crowdbotics <team@crowdbotics.com>
2020-09-21 05:35:42 +0000

    Update models

--- texas_personal_20495/wsgi.py
+++ texas_personal_20495/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'texas_personal_20495.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "texas_personal_20495.settings")
 
 application = get_wsgi_application()
